55f9a1284e0377cdf8a981f5ba74005e849390a0
Michael Dimitras <michaeladimitras@gmail.com>
2017-09-08 19:31:46 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,32 +1,32 @@
 var chocolateBars = [`snickers`, `hundred grand`, `kitkat`, `skittles`]
 
-function addElementToBeginningOfArray(arr, e){
-  return [e,...arr];
-}
-
-
-function destructivelyAddElementToBeginningOfArray(arr,e) {
-  return arr.push(e);
-}
-
-
-function accessElementInArray(arr, n){
-  return arr[n];
-}
-
-function destructivelyRemoveElementFromBeginningOfArray(arr){
-  return arr.shift();
-}
-
-function removeElementFromBeginningOfArray(arr){
-  return arr.slice(1);
-}
-
-
-function destructivelyRemoveElementFromEndOfArray(arr){
-  return arr.pop();
-}
-
-function removeElementFromEndOfArray(arr){
-  return arr.slice(-1);
-}
+// function addElementToBeginningOfArray(arr, e){
+//   return [e,...arr];
+// }
+//
+//
+// function destructivelyAddElementToBeginningOfArray(arr,e) {
+//   return arr.push(e);
+// }
+//
+//
+// function accessElementInArray(arr, n){
+//   return arr[n];
+// }
+//
+// function destructivelyRemoveElementFromBeginningOfArray(arr){
+//   return arr.shift();
+// }
+//
+// function removeElementFromBeginningOfArray(arr){
+//   return arr.slice(1);
+// }
+//
+//
+// function destructivelyRemoveElementFromEndOfArray(arr){
+//   return arr.pop();
+// }
+//
+// function removeElementFromEndOfArray(arr){
+//   return arr.slice(-1);
+// }
