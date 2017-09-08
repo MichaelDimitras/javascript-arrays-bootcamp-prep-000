7a7dd137a8e1d160c26fd6c24e145b82c854324a
Michael Dimitras <michaeladimitras@gmail.com>
2017-09-08 19:32:19 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -17,12 +17,12 @@ function accessElementInArray(arr, n){
 function destructivelyRemoveElementFromBeginningOfArray(arr){
   return arr.shift();
 }
-//
-// function removeElementFromBeginningOfArray(arr){
-//   return arr.slice(1);
-// }
-//
-//
+
+function removeElementFromBeginningOfArray(arr){
+  return arr.slice(1);
+}
+
+
 // function destructivelyRemoveElementFromEndOfArray(arr){
 //   return arr.pop();
 // }
